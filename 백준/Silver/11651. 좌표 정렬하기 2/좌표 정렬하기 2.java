b5c584fbedba4d5ccaa1dacd834148af12fac5ef
CollectCode <141992680+CollectCode@users.noreply.github.com>
2024-05-09 23:45:40 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 888 ms, Memory: 60432 KB -BaekjoonHub

---
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.java
@@ -0,0 +1,38 @@
+import java.io.BufferedWriter;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.io.IOException;
+
+public class Main   {
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        int num = Integer.parseInt(br.readLine());
+        int[][] list = new int[num][2];
+
+        for (int j = 0; j < num; j++)   {
+            String[] str = br.readLine().split(" ");
+            list[j][0] = Integer.parseInt(str[0]);
+            list[j][1] = Integer.parseInt(str[1]);
+        }
+        
+        Arrays.sort(list, (p1, p2) ->   {
+            if(p1[1] == p2[1])  {
+                return p1[0] - p2[0];
+            }
+            else {
+                return p1[1] - p2[1];
+            }
+        });
+
+        for (int i = 0; i < num; i++)   {
+            bw.write(list[i][0] + " ");
+            bw.write(list[i][1] + "\n");
+        }
+        bw.flush();
+        bw.close();
+    }
+}
