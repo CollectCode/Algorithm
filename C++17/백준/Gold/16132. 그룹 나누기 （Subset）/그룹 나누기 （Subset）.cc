19c45e65e279af673f0e526e05e30990af1199d0
CollectCode <141992680+CollectCode@users.noreply.github.com>
2024-08-31 13:19:37 +0900

    [Gold IV] Title: 그룹 나누기 (Subset), Time: 0 ms, Memory: 1504 KB, Score: 25 point -BaekjoonHub

---
+++ C++17/백준/Gold/16132. 그룹 나누기 （Subset）/그룹 나누기 （Subset）.cc
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<algorithm>
+#define RANGE 100000
+using namespace std;
+int in[RANGE]={1},e=0;
+int main()
+{
+	int num,sum=0;
+	scanf("%d",&num);
+	sum=(num*(num+1))/2;
+	if(sum%2==1) 
+	{
+		printf("0"); 
+		exit(0);
+	}
+
+	for(int i=1;i<=num;i++)
+	{
+		for(int j=i+e;j>=1;j--)
+		{
+			if(j==i+e) e=j;
+			if(j-i>=0) in[j]=in[j]+in[j-i];
+		}
+	}
+	printf("%d",in[sum/2]/2);
+	return 0;
+}
