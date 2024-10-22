7355b1e5d187521d358d832fce752adc9a0db103
CollectCode <141992680+CollectCode@users.noreply.github.com>
2024-10-22 20:53:39 +0900

    [Silver III] Title: N과 M (1), Time: 280 ms, Memory: 33692 KB -BaekjoonHub

--- Java/백준/Silver/15649. N과 M （1）/N과 M （1）.java
+++ Java/백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -7,28 +7,33 @@ public class Main {
     private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     private static StringTokenizer st;
     private static int n, m;
+    private static StringBuilder sb = new StringBuilder("");
     private static int chk[] = new int[9];
-    private static StringBuilder sb = new StringBuilder();
-    private static void process(int x) {
-        if(x>m) {
-            System.out.println(sb.toString().trim());
+    private static int inp[] = new int[9];
+    private static void process(int dep)    {
+        if(dep==m)  {
+            for(int i=0;i<m;i++) {
+                if(chk[inp[i]]==1)   {
+                    sb.append(inp[i] + " ");
+                }
+            }
+            sb.append("\n");
             return;
         }
-        for (int i = 1; i <= n; i++) {
-            if (chk[i] == 0) {
-                chk[i] = 1;
-                sb.append(i + " ");
-                process(x + 1);
-                sb.setLength(sb.length()-2);
-                chk[i] = 0;
+        for(int i=1;i<=n;i++)   {
+            if(chk[i]==0)   {
+                inp[dep]=i;
+                chk[i]=1;
+                process(dep+1);
+                chk[i]=0;
             }
         }
     }
-
     public static void main(String args[]) throws IOException {
         st = new StringTokenizer(br.readLine());
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
-        process(1);
+        process(0);
+        System.out.println(sb);
     }
 }
