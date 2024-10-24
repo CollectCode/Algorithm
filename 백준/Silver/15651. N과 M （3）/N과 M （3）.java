4d68e3174c527e6504aba1d39e9fc1525a1023fc
CollectCode <141992680+CollectCode@users.noreply.github.com>
2024-10-25 00:13:50 +0900

    [Silver III] Title: N과 M (3), Time: 540 ms, Memory: 207932 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    private static StringTokenizer st;
+    private static int n, m;
+    private static StringBuilder sb = new StringBuilder("");
+    private static int inp[] = new int[9];
+    private static void process(int dep) {
+        if (dep == m) {
+            for (int i = 0; i < m; i++) {
+                sb.append(inp[i] + " ");
+            }
+            sb.append("\n");
+            return;
+        }
+        for (int i = 1; i <= n; i++) {
+            inp[dep] = i;
+            process(dep+1);
+        }
+    }
+    public static void main(String args[]) throws IOException {
+        st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        process(0);
+        System.out.println(sb);
+    }
+}
